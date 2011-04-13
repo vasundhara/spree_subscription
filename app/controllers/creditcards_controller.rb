b5e7bc9aef15cc4a26a3f901093b0dfffa956440
jeffmcfadden <jeff.mcfadden@gmail.com>
2011-04-13 13:07:36 -0700

    CC Controller update

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -10,6 +10,14 @@ class CreditcardsController < ApplicationController
   #		
   #		SubscriptionMailer.deliver_subscription_reactivated(@subscription) 
     end
+
+    # cim_gateway gets us the actual AuthorizeNetCIM from ActiveMerchant
+    # and we have to delete the old profile because we don't want to
+    # accidentally create a duplicate
+    Gateway.cim_gateway.delete_customer_profile( self.gateway_customer_profile_id ) unless self.gateway_customer_profile_id.nil?
+
+    # Create the payment profile for this card
+    Gateway.current.create_profile_from_card( self )
   end
 
 	private
