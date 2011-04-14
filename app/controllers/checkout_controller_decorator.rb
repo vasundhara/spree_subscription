2848542d678ec8fc70cceff4ea9716c500abf857
Ben Hutton <benhutton@gmail.com>
2011-04-14 12:39:14 -0400

    Force sign in if you add a subscribable in the middle of guest checkout

---
+++ app/controllers/checkout_controller_decorator.rb
@@ -0,0 +1,15 @@
+CheckoutController.class_eval do
+  before_filter :check_subscriptions_for_registration, :except => [:registration, :update_registration]
+
+  private
+
+  # Go to the registration step if any subscribables and no current_user
+  def check_subscriptions_for_registration
+    return unless Spree::Auth::Config[:registration_step]
+    return if current_user
+    if current_order.contains_subscription?
+      store_location
+      redirect_to checkout_registration_path
+    end
+  end
+end
