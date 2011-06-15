2a7b56a55efceb18284076fcaf4705bd4a329569
Gagan Awhad <gagan.awhad@desiringgod.org>
2011-06-14 20:55:54 -0500

    A bunch of changes

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -1,17 +1,20 @@
 class CreditcardsController < ApplicationController
-  before_filter :load_data, :only => :update
+  before_filter :load_data, :only => [:update, :create]
   resource_controller
 
-  update.success.wants.html { redirect_to subscription_url(@subscription) }
-  create.success.wants.html { redirect_to subscription_url(@subscription) }
+  update.success.wants.html { redirect_to subscription_path(@subscription) }
+  create.success.wants.html { redirect_to subscription_path(@subscription) }
 
   create.after do
     # cim_gateway gets us the actual AuthorizeNetCIM from ActiveMerchant
     # and we have to delete the old profile because we don't want to
     # accidentally create a duplicate
-    @subscription.is_arb?
+    if @subscription.is_arb?
+      object.address = @subscription.legacy_address
+      @subscription.creditcard = object
+      @subscription.save
       gateway = Gateway.find(:first, :conditions => {:type => "Gateway::AuthorizeNetCim", :active => true, :environment => Rails.env})
-      gateway.delete_customer_profile( @subscription.creditcard.gateway_customer_profile_id ) unless @subscription.creditcard.gateway_customer_profile_id.nil?
+      gateway.send(:cim_gateway).delete_customer_profile( @subscription.creditcard.gateway_customer_profile_id ) unless @subscription.creditcard.gateway_customer_profile_id.nil?
 
       # Create the payment profile for this card
       gateway.create_profile_from_card( @subscription.creditcard )
