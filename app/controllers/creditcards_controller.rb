749f2a243c0864dc10c4e7ec6849a48401cc2ad6
Gagan Awhad <gagan.awhad@desiringgod.org>
2011-06-14 19:32:10 -0500

    Checking to see if the subscription is an arb before creating a cim profile

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -9,11 +9,13 @@ class CreditcardsController < ApplicationController
     # cim_gateway gets us the actual AuthorizeNetCIM from ActiveMerchant
     # and we have to delete the old profile because we don't want to
     # accidentally create a duplicate
-    gateway = Gateway.find(:first, :conditions => {:type => "Gateway::AuthorizeNetCim", :active => true, :environment => Rails.env})
-    gateway.delete_customer_profile( @subscription.creditcard.gateway_customer_profile_id ) unless @subscription.creditcard.gateway_customer_profile_id.nil?
+    @subscription.is_arb?
+      gateway = Gateway.find(:first, :conditions => {:type => "Gateway::AuthorizeNetCim", :active => true, :environment => Rails.env})
+      gateway.delete_customer_profile( @subscription.creditcard.gateway_customer_profile_id ) unless @subscription.creditcard.gateway_customer_profile_id.nil?
 
-    # Create the payment profile for this card
-    gateway.create_profile_from_card( @subscription.creditcard )
+      # Create the payment profile for this card
+      gateway.create_profile_from_card( @subscription.creditcard )
+    end
   end
 
   private
