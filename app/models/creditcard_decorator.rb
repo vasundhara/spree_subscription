0f462fa187b4bb5720060057cf4e53eedeba000e
Gagan Awhad <gagan.awhad@desiringgod.org>
2011-07-05 19:37:31 -0500

    looks like set_last_digit is set to accept last_digits from forms directly. But incase we want to update the credticard number, it should still be able to change the last digit. This changes allows for that

--- app/models/creditcard_decorator.rb
+++ app/models/creditcard_decorator.rb
@@ -1,5 +1,11 @@
 Creditcard.class_eval	do
   has_one :subscription
   belongs_to :address
+  before_update :update_last_digits
   accepts_nested_attributes_for :address, :allow_destroy => true, :reject_if => proc { |attrs| attrs['firstname'].blank? }
+
+  def update_last_digits
+    number.to_s.gsub!(/\s/,'') unless number.nil?
+    self.last_digits = number.to_s.length <= 4 ? number : number.to_s.slice(-4..-1) unless number.blank? 
+  end
 end
