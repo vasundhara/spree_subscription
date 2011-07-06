9bdcadd4fc679387a0462bdc32f4204c0fdd4990
Gagan Awhad <gagan.awhad@desiringgod.org>
2011-07-05 19:34:41 -0500

    Creditcard now accepts attributes for Address

--- app/models/creditcard_decorator.rb
+++ app/models/creditcard_decorator.rb
@@ -1,4 +1,5 @@
 Creditcard.class_eval	do
   has_one :subscription
   belongs_to :address
+  accepts_nested_attributes_for :address, :allow_destroy => true, :reject_if => proc { |attrs| attrs['firstname'].blank? }
 end
