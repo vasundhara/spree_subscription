472a373a6912652324a5d98acd15b20cfab1f354
Gagan Awhad <gagan.awhad@desiringgod.org>
2011-07-08 13:39:34 -0500

    Creditcards controller, the way I would like it be. Lots of refactoring done - almost like writing a new controller

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -1,24 +1,56 @@
 class CreditcardsController < ApplicationController
-  before_filter :load_data, :only => [:update, :create]
-  resource_controller
+  before_filter :load_data, :only => [:new, :update, :create]
+  rescue_from Spree::GatewayError, :with => :rescue_from_spree_gateway_error
+  #resource_controller
+  #before_update :hello
 
-  update.success.wants.html { redirect_to subscription_path(@subscription) }
-  create.success.wants.html { redirect_to subscription_path(@subscription) }
+  def new
+    @creditcard = Creditcard.new
+    @address = @creditcard.build_address
+  end
+  def edit
+    @creditcard = Creditcard.find(params[:id])
+  end
 
-  create.after do
+  def update
+    @creditcard = Creditcard.find(params[:id])
+    gateway = Gateway.find(:first, :conditions => {:type => "Gateway::AuthorizeNetCim", :active => true, :environment => Rails.env})
+    gateway.create_customer_payment_profile_from_card(@creditcard)
+    if @creditcard.update_attributes(params[:creditcard])
+      @creditcard.address.save if @creditcard.address #NOTE for some reason update_attributes is not saving the address to the database :( :( :(
+      if subscription.state != 'active'
+        @subscription.reactivate
+      end
+      flash[:notice] = "Payment method for subscription was updated successfully"
+      redirect_to subscription_path(@subscription) 
+    else
+      flash[:error]  = "There was a problem updating payment method for this subscription. Please try again"
+      redirect_to = edit_subscription_creditcard(@creditcard)
+    end
+  end
+
+
+  #update.success.wants.html { redirect_to subscription_path(@subscription) }
+  #create.success.wants.html { redirect_to subscription_path(@subscription) }
+
+  def create 
     # cim_gateway gets us the actual AuthorizeNetCIM from ActiveMerchant
     # and we have to delete the old profile because we don't want to
     # accidentally create a duplicate
-    if @subscription.is_arb?
-      object.address = @subscription.legacy_address
-      object.save
-      @subscription.creditcard = object
-      @subscription.save
-      gateway = Gateway.find(:first, :conditions => {:type => "Gateway::AuthorizeNetCim", :active => true, :environment => Rails.env})
-
-      # Create the payment profile for this card
-      gateway.create_profile_from_card( @subscription.creditcard )
-      @subscription.migrate_arb_to_cim
+    @creditcard = @subscription.build_creditcard(params[:creditcard])
+    @creditcard.address = @subscription.legacy_address
+    if @subscription.save
+      if @subscription.is_arb?
+        gateway = Gateway.find(:first, :conditions => {:type => "Gateway::AuthorizeNetCim", :active => true, :environment => Rails.env})
+        # Create the payment profile for this card
+        gateway.create_profile_from_card( @subscription.creditcard )
+        @subscription.migrate_arb_to_cim
+      end
+      flash[:notice] = "Payment method for subscription was updated successfully"
+      redirect_to subscription_path(@subscription) 
+    else
+      flash[:error]  = "There was a problem updating payment method for this subscription. Please try again"
+      redirect_to = edit_subscription_creditcard(@creditcard)
     end
   end
 
@@ -26,4 +58,10 @@ class CreditcardsController < ApplicationController
   def load_data
     @subscription = Subscription.find(params[:subscription_id])
   end
+
+ def rescue_from_spree_gateway_error
+    flash[:error] = t('spree_gateway_error_flash_for_checkout')
+    render :edit
+  end
+  
 end
